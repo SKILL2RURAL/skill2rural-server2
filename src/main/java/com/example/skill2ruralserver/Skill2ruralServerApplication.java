5379b77946ae3f965c6680aba8d25ef2a3f53154
Precious Adedibu <adedibuprecious@gmail.com>
2024-12-07 00:12:46 +0100

    initialized app

---
+++ src/main/java/com/example/skill2ruralserver/Skill2ruralServerApplication.java
@@ -0,0 +1,13 @@
+package com.example.skill2ruralserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Skill2ruralServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Skill2ruralServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/skill2ruralserver/Skill2ruralServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.skill2ruralserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Skill2ruralServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
